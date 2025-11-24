2d1f00e982ca883c75ae1719ed62218893fe16a5
Bfdiego <dayo.alba.p@gmail.com>
2025-11-24 08:35:57 -0400

    Hola

--- backend/app/main.py
+++ backend/app/main.py
@@ -13,7 +13,7 @@ def custom_generate_unique_id(route: APIRoute) -> str:
 
 if settings.SENTRY_DSN and settings.ENVIRONMENT != "local":
     sentry_sdk.init(dsn=str(settings.SENTRY_DSN), enable_tracing=True)
-
+#jhola
 app = FastAPI(
     title=settings.PROJECT_NAME,
     openapi_url=f"{settings.API_V1_STR}/openapi.json",
